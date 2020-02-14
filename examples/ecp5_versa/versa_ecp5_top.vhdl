2117e1d0e50bfa59264ae1e5f557e515b40613b2
Martin <hackfin@section5.ch>
2020-02-14 08:14:28 +0100

    Added a bit of description in source

--- examples/ecp5_versa/versa_ecp5_top.vhdl
+++ examples/ecp5_versa/versa_ecp5_top.vhdl
@@ -4,6 +4,14 @@
 --
 -- 1/2017  Martin Strubel <hackfin@section5.ch>
 --
+-- Taken from MaSoCist and stripped down for the example.
+-- Functionality:
+
+-- * Blinks the first orange LED every second
+-- * Loops back the UART (fixed at 115200 bps) through a FIFO and
+--   turns lower cap alpha characters into upper case.
+
+
 library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
@@ -16,12 +24,6 @@ entity versa_ecp5_top is
         CLK_FREQUENCY : positive := 25000000
     );
 	port (
-		-- clk_out     : out  std_logic;
-
-		-- spi_miso  : in  std_logic;
-		-- spi_mosi  : out std_logic;
-		-- spi_cs    : out std_logic;
-
 		twi_scl  : inout std_logic;
 		twi_sda  : inout std_logic;
 
@@ -34,7 +36,6 @@ entity versa_ecp5_top is
 		dip_sw       : in   std_logic_vector(7 downto 0);
 
 		reset_n      : in   std_logic;
-		-- clk_serdes   : in   std_logic;
 		clk_in       : in   std_ulogic
 
 	);
@@ -50,15 +51,6 @@ architecture behaviour of versa_ecp5_top is
 	signal pclk           : std_logic;
 
 	signal comb_reset     : std_logic;
-
-	-- signal spi_sclk       : std_logic;
-	-- signal spi_ts         : std_logic;
-	-- signal spi_cs_int     : std_logic;
-
-	-- GPIOs:
-	-- Set to defined state for simulation:
-	-- signal gpio           : unsigned(31 downto 0);
-	-- signal cfg            : std_logic_vector(7 downto 0);
 
 	constant f_half : integer := CLK_FREQUENCY / 2;
 	signal reset_delay    : unsigned(3 downto 0);
@@ -79,12 +71,8 @@ begin
 
 	comb_reset <= (not reset_n) or (not mclk_locked);
 
-	-- seg <= not std_logic_vector(gpio(13 downto 0));
-	-- segdp <= not gpio(14);
 	seg <= (others => '1'); -- low active
 	segdp <= '1'; -- low active
-
-	-- gsr_inst: GSR port map (reset_n);
 
 	process(mclk)
     begin
@@ -96,24 +84,6 @@ begin
             end if;
         end if;
     end process;
-
--- We run a delayed reset for the CPU, as there might be race conditions
--- with the TAP.
-
-delayed_cpu_reset:
-	process (comb_reset, mclk)
-	begin
-		if comb_reset = '1' then
-			reset_delay	<= x"f";
-			-- delayed_reset <= '1';
-		elsif rising_edge(mclk) then
-			if reset_delay /= x"0" then
-				reset_delay <= reset_delay - 1;
-			else
-				-- delayed_reset <= '0';
-			end if;
-		end if;
-	end process;
 
 
 clk_pll1: entity work.pll_mac
