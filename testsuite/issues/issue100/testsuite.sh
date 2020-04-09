6fa44a30cb7de59aaba22b0b77ec3a26a71ffb56
Martin <hackfin@section5.ch>
2020-04-09 11:47:32 +0200

    Elaborate better on issue #100 (dual instance)
    - Use -defer option
    - Added dual instance test case in script

--- testsuite/issues/issue100/testsuite.sh
+++ testsuite/issues/issue100/testsuite.sh
@@ -3,8 +3,18 @@
 topdir=../..
 . $topdir/testenv.sh
 
-run_yosys -q -p "ghdl top.vhdl ../../../library/libram.vhdl -e top;
-	read_verilog ../../../library/verilog/dpram16_init.v;
+# Test case: make sure to use -defer option for passed INIT_HEX
+# option, and that no mem.hex is around (reference to it should
+# be replaced by INIT_HEX parameter
+# This case only tests that hierarchy resolves
+[ -e mem.hex ] && rm mem.hex
+run_yosys -p "ghdl top.vhdl ../../../library/libram.vhdl -e top;
+	read_verilog -defer ../../../library/verilog/dpram16_init.v;
+	hierarchy -check; ls"
+
+# FAILURE case: Re-definition of module `\DPRAM16_init_hex_ce
+run_yosys -q -p "ghdl -gDUAL_INST=true top.vhdl ../../../library/libram.vhdl -e top;
+	read_verilog -defer ../../../library/verilog/dpram16_init.v;
 	hierarchy -check; ls"
 
 clean
